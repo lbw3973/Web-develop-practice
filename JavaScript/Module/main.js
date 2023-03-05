26efb3791706d14240571789ca62bab66115b964
LeeByoungWook <lbw3973@naver.com>
2023-03-05 19:21:30 +0900

    Feat: JS module 내용 추가

---
+++ JavaScript/Module/main.js
@@ -0,0 +1,12 @@
+import * as abc from './module.js'
+
+// import a, {world, hello, good} from './module.js' 
+
+// import a from './module.js'
+// import {world} from './module.js'
+
+// console.log(a)
+// console.log(world)
+// console.log(hello)
+// console.log(good)
+console.log(abc)
---
+++ JavaScript/Module/module.js
@@ -0,0 +1,5 @@
+export default 123
+
+export const hello = 1
+export const world = 2
+export const good = 3
